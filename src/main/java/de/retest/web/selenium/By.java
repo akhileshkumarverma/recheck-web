467059609caca3be1873ed8509c8a2447b2b453d
Jeremias Roessler <roessler@retest.de>
2019-05-16 14:56:53 +0200

    p: We need to set the retestId ... but that can only be done after a release

--- src/main/java/de/retest/web/selenium/By.java
+++ src/main/java/de/retest/web/selenium/By.java
@@ -24,9 +24,11 @@ public abstract class By extends org.openqa.selenium.By {
 			return null;
 		}
 		final Alignment alignment = Alignment.createAlignment( lastExpectedState, lastActualState );
-		final Element actualElement = alignment.get( result );
-		// TODO Once we have recheck 1.1.0 released, do that:
-		actualElement.applyRetestId( result.getRetestId() );
+		Element actualElement = alignment.get( result );
+		if ( actualElement != null ) {
+			// TODO Once we have recheck 1.1.0 released, do that:
+			actualElement = actualElement.applyRetestId( result.getRetestId() );
+		}
 		return actualElement;
 	}
 
